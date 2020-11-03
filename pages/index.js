82a3450ab5ef334e12766f12ed1ea56edfa33933
Alessandro Maruccia <alessandro.maruccia@gig.com>
2020-11-03 17:14:00 +0100

    add background

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
 import Typewriter from "../components/typewriter";
 import Head from "next/head";
+import { useEffect } from "react";
 
 export default function IndexPage() {
+  useEffect(() => {
+    VANTA.NET({
+      el: "body",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x343434,
+      backgroundColor: 0x0,
+      points: 10.00,
+      maxDistance: 20.00,
+      spacing: 15.00
+    })
+  })
   return (
     <div className="content">
       <Head>
@@ -10,6 +28,8 @@ export default function IndexPage() {
         <meta name="robots" content="index, follow" />
         <meta rel="canonical" href="https://alessandromaruccia.com/" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script>
       </Head>
       <h1 className="title">
         <Typewriter text="Alessandro Maruccia" />
@@ -26,9 +46,8 @@ export default function IndexPage() {
       <br />
       I’m currently employed as Senior Front End Developer at Gaming Innovation
       Group in Marbella, Spain. <br />
-      If you wish, you can get in touch with me via email at{" "}
-        <a href="mailto:alesmaru@gmail.com">alesmaru@gmail.com</a>
-    </p>
+      If you wish, you can get in touch with me via email at{" "}<a href="mailto:alesmaru@gmail.com">alesmaru@gmail.com</a>
+      </p>
 
       <p className="hero">
         Check out my latest projects: <a href="https://www.barrildelgusto.com" target="_blank">El Barril del Gusto</a>, <a href="https://www.happyandalusia.com" target="_blank">Happy Andalusia</a>
